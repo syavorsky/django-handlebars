c3e5fe61f25a4b8ccb510ae65b0f0857f64dcd98
Sergii @devbox <yavorskiy.s@gmail.com>
2012-03-14 11:54:10 -0700

    misc fixes

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ META = dict(
     include_package_data=True,
     zip_safe=False,
     classifiers=[
+        "Development Status :: 4 - Beta",
         "Environment :: Web Environment",
         "Framework :: Django",
         "Intended Audience :: Developers",
