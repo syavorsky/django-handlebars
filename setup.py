d490b2c2146186b1d68df519090facbc4856e1c6
Sergii @devbox <yavorskiy.s@gmail.com>
2012-03-14 10:01:19 -0700

    setup.py is done

--- setup.py
+++ setup.py
@@ -2,33 +2,38 @@
 #from distutils.core import setup
 from setuptools import setup, find_packages
 
-
-META_DATA = dict(
-    name='django-handlebars',
-    version='0.1',
-    description='Handlebars for Django',
-    long_description=open('README').read(),
-    author='Sergii Iavorskyi',
-    author_email='yavorskiy.s@gmail.com',
-    url='https://github.com/yavorskiy/django_handlebars',
-    keywords='django javascript test',
-    classifiers=[
-        "Development Status :: 4 - Beta",
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: JavaScript',
-        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
-        'Topic :: Software Development :: Testing',
-    ],
-    install_requires= ['Django>=1.3', ],
+META = dict(
+    name="django-handlebars",
+    version="0.1",
+    description="Handlebars for Django",
+    long_description=open("README.md").read(),
+    author="Sergii Iavorskyi",
+    author_email="yavorskiy.s@gmail.com",
+    url="https://github.com/yavorskiy/django_handlebars",
+    download_url="https://github.com/yavorskiy/django-handlebars/downloads",
+    license="BSD",
+    keywords="django handlebars",
     packages=find_packages(exclude=[]),
+    install_requires = ["django>=1.3", ],
+    extras_require = {
+        "Compile templates": ["python-spidermonkey",],
+        "Compile templates on chnges": ["python-spidermonkey", "pyinotify"],
+    },
+    test_suite="runtests.runtests",
     include_package_data=True,
     zip_safe=False,
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "Environment :: Web Environment",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: JavaScript",
+        "Topic :: Internet :: WWW/HTTP :: Dynamic Content",
+        "Topic :: Software Development :: Testing",
+    ],
 )
 
-
 if __name__ == "__main__":
-    setup(**META_DATA)
+    setup(**META)
